0cd324637f33d153df56d4d783fb0bd11d8222fd
alessiogentilezza <alessio.gentilezza@gmail.com>
2023-03-10 17:43:12 +0100

    snack 2 undefined

--- js/main.js
+++ js/main.js
@@ -6,8 +6,10 @@ Stampare a schermo la bici con peso minore utilizzando destructuring e template 
 Snack2
 Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
 Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+
 Generare numeri random al posto degli 0 nelle proprietà:
 Punti fatti e falli subiti.
+
 Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
 
 BONUS
@@ -15,6 +17,8 @@ Snack 3
 Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b).
 La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
 Usiamo i nuovi metodi degli array foreach o filter. */
+
+/***********************SNACK 1 */
 
 const biciclettaDaCorsa = [
     {
@@ -44,3 +48,58 @@ console.log(biciLeggera);
 
 let { nome, peso } = biciLeggera;
 console.log(`${nome} pesando ${peso} kg è la bicicletta più legera`);
+
+
+/***********************SNACK 2 */
+
+const squadreDiCalcio = [
+    {
+        "nome_squadra": "lazio",
+        "punti_fatti": 0,
+        "falli_subiti": 0
+    },
+    {
+        "nome_squadra": "roma",
+        "punti_fatti": 0,
+        "falli_subiti": 0
+    },
+    {
+        "nome_squadra": "fiorentina",
+        "punti_fatti": 0,
+        "falli_subiti": 0
+    }
+];
+
+squadreDiCalcio.forEach((elemento) => {
+
+    if (elemento.punti_fatti == 0) {
+        elemento.punti_fatti = getNumeriCasualiUnici(1, 1, 100);
+    } else if (elemento.falli_subiti == 0) {
+        elemento.falli_subiti = getNumeriCasualiUnici(1, 1, 100);
+    }
+});
+
+console.log(squadreDiCalcio);
+
+let { nome_squadra, falli_subiti } = squadreDiCalcio;
+console.log(`La ${nome_squadra} ha subito ${falli_subiti} falli`);
+
+
+
+
+
+
+
+function getNumeriCasualiUnici(numeroElementi, min, max) {
+
+    const numeri = [];
+
+    while (numeri.length < numeroElementi) {
+        const numeroCasuale = Math.floor(Math.random() * (max - min + 1)) + min;
+        if (!numeri.includes(numeroCasuale)) {
+            numeri.push(numeroCasuale);
+        }
+    }
+    return numeri;
+
+}
